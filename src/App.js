78a2c103bf8cc65cf56bb9cdb69b42f9a4b04d7d
Tyler <tylerjmarcott@gmail.com>
2024-01-27 17:44:34 -0700

    copied css for project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
 import React from "react";
+import { useEffect } from "react";
+
+
+
+const API_URL = 'http://www.omdbapi.com?apikey=28b65a8c'
 
 const App = () => {
+
+  const searchMovies = async (title) => {
+    const res = await fetch(`${API_URL}&s=${title}`)
+    const data = await res.json();
+
+    console.log(data.Search);
+  }
+
+  useEffect(() => {
+    searchMovies('hereditary');
+  }, []);
+
   return(
     <h1>APPPPPPPPPPPPP</h1>
   );
